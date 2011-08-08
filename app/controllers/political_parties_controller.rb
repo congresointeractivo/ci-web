04241bb99473a524f9774a27ce360d672d727287
etagwerker <ernesto@etagwerker.com>
2011-08-08 01:35:14 -0300

    Agregué controller para partidos y proyectos.

---
+++ app/controllers/political_parties_controller.rb
@@ -0,0 +1,24 @@
+class PoliticalPartiesController < ApplicationController
+  # GET /political_parties
+  # GET /political_parties.xml
+  def index
+    @political_parties = PoliticalParty.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @political_parties }
+    end
+  end
+
+  # GET /political_parties/1
+  # GET /political_parties/1.xml
+  def show
+    @political_party = PoliticalParty.find(params[:id])
+    @miembros = @political_party.miembros.paginate :page => params[:page], :per_page => 50
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @political_party }
+    end
+  end
+end
---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,23 @@
+class ProjectsController < ApplicationController
+  # GET /projects
+  # GET /projects.xml
+  def index
+    @projects = Project.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @projects }
+    end
+  end
+
+  # GET /projects/1
+  # GET /projects/1.xml
+  def show
+    @project = Project.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @project }
+    end
+  end
+end
