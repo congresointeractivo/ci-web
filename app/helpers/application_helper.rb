c98b229e3cc46a088918c707624f6e4e5b820d57
etagwerker <ernesto@etagwerker.com>
2011-08-08 01:35:27 -0300

    Agregué helpers para las vistas con datos en nil.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,40 @@
+# encoding: utf-8
+
 module ApplicationHelper
+  
+  # Muestra el titulo o un mensaje que no hay titulo
+  def display_project_title(project)
+    if project.title.blank?
+      "(Sin Título)"      
+    else
+      project.title
+    end
+  end
+  
+  # Muestra el sumario o un mensaje que no hay sumario
+  def display_project_summary(project)
+    if project.summary.blank?
+      "(Sin Sumario)"      
+    else
+      project.summary
+    end
+  end
+  
+  # AFIRMATIVE
+  # ABSENT
+  # NEGATIVE
+  def display_vote_type(type)
+    case type.to_sym
+      when :AFIRMATIVE
+        "Positivo"
+      when :ABSENT
+        "Ausente"
+      when :NEGATIVE
+        "Negativo"
+      else
+        type
+    end
+    
+  end
+
 end
---
+++ app/helpers/political_parties_helper.rb
@@ -0,0 +1,2 @@
+module PoliticalPartiesHelper
+end
