d7c5b7e48be31d42793684aeb3cf15f1e6b7604b
etagwerker <ernesto@etagwerker.com>
2011-08-21 20:54:15 -0300

    Agregué filtros por partido para diputados y senadores.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery  
+  
+  protected
+  
+  # picks up the filter from params[:bloques]
+  # loads bloques to be used in filtering
+  def setup_filters
+    @filter = {}
+    @filter[:bloques] = params[:bloques]    
+    @bloques = PoliticalParty.where(:type => params[:controller].upcase).all
+  end
+  
+  # sorts and paginates by default the legislators
+  def sort_and_paginate_legislators(legislators)
+    legislators.all(:include => [:politician], :order => 'Politician.lastName ASC').paginate :page => params[:page], :per_page => PER_PAGE  
+  end
 end
--- app/controllers/diputados_controller.rb
+++ app/controllers/diputados_controller.rb
@@ -1,8 +1,11 @@
 class DiputadosController < ApplicationController
+  before_filter :setup_filters, :only => [:filter]
+  
   # GET /diputados
   # GET /diputados.xml
   def index
-    @diputados = Diputado.all(:include => [:politician], :order => 'Politician.lastName ASC').paginate :page => params[:page], :per_page => 50
+    @bloques = PoliticalParty.where(:type => "DIPUTADOS").all    
+    @diputados = Legislador.diputados.all(:include => [:politician], :order => 'Politician.lastName ASC').paginate :page => params[:page], :per_page => 50
 
     respond_to do |format|
       format.html # index.html.erb
@@ -19,5 +22,24 @@ class DiputadosController < ApplicationController
       format.html # show.html.erb
       format.xml  { render :xml => @diputado }
     end
+  end
+  
+  # GET /diputados/filtrar_por
+  def filter
+    if @filter[:bloques]
+      @parties = PoliticalParty.get_parties(@filter[:bloques])
+      
+      if @parties.count > 1
+        @diputados = Legislador.diputados.for_parties(@parties)
+      else
+        @diputados = Legislador.diputados.for_party(@parties.first)
+      end
+    else
+      @diputados = Legislador.diputados
+    end
+
+    @diputados = sort_and_paginate_legislators(@diputados)
+
+    render :action => 'index'
   end
 end
