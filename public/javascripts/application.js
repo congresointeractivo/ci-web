2b0a236387daa7256fcb136ea08b0c97fbe0fbb0
etagwerker <ernesto@etagwerker.com>
2011-08-21 20:54:45 -0300

    Agregué jquery chosen para filtrar legisladores.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  $(this).find('.bloques-chosen-select').data("placeholder","Filtrar por Bloque").chosen();
+  
+  $(this).find('.bloques-chosen-select').chosen().change(function() {
+    var $this = $(this);
+    var url = window.location.href.split('?', 2)[0];
+
+    url 
+    
+    if (url.indexOf("filtrar_por") == -1) {
+      url = url + '/filtrar_por'
+    }
+
+    $this.val() ?
+      window.location.href = url + '?' + $this.attr('name') + '=' + $this.val().join(',') :
+      window.location.href = url
+  });
+  
+});
