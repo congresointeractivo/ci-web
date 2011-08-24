a7c04eef00e795e3dd922ce17ce46ae43a715e12
etagwerker <ernesto@etagwerker.com>
2011-08-24 16:18:02 -0300

    Agregué paginación a proyectos, porque hay 60.000.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.xml
   def index
-    @projects = Project.all
+    @projects = Project.all.paginate :page => params[:page], :per_page => PER_PAGE  
 
     respond_to do |format|
       format.html # index.html.erb
