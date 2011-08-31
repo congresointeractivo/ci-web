0229f11c60f30ade663a878fce426a191da60449
etagwerker <ernesto@etagwerker.com>
2011-08-31 20:34:29 -0300

    Agregué proyectos al layout. Agregué to param al project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,12 @@ class Project < ActiveRecord::Base
   has_many :project_elements
   
   has_and_belongs_to_many :votes, :foreign_key => 'Project_id', :join_table => 'Project_Vote', :association_foreign_key => 'voters_id'  
+  
+  def to_param
+    if self.title
+      "#{id}-#{self.title.gsub(/[^a-z0-9]+/i, '-')}"      
+    else
+      id
+    end
+  end
 end
