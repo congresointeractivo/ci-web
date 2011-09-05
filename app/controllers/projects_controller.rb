d332752589a328b9b39fc22944f2f5c45eb02beb
Jose Allona <jallona@gmail.com>
2011-09-04 23:41:05 -0300

    cambié consultas en la vista por variables

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.xml
   def index
-    @projects = Project.all.paginate :page => params[:page], :per_page => PER_PAGE  
+    @projects = Project.all(:order => 'Project.initiated DESC').paginate :page => params[:page], :per_page => PER_PAGE  
 
     respond_to do |format|
       format.html # index.html.erb
@@ -14,7 +14,10 @@ class ProjectsController < ApplicationController
   # GET /projects/1.xml
   def show
     @project = Project.find(params[:id])
-
+    @afirmative_vote_count = @project.votes.where(:voteType => 'AFIRMATIVE').count
+    @negative_vote_count = @project.votes.where(:voteType => 'NEGATIVE').count
+    @absent_vote_count = @project.votes.where(:voteType => 'ABSENT').count
+    @abstention_vote_count = @project.votes.where(:voteType => 'ABSTENTION').count
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @project }
