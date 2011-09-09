18bcb27574aae62dcb1a8134ca547574c7b2067e
Sebastian Zelonka <sebastianzelonka@gmail.com>
2011-09-09 00:13:36 -0300

    Arreglé los problemas que se habian generado en la lógica para mostrar Twitter.

---
+++ public/javascripts/modales.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	var h = $(window).height()*.8;
+	var w = $(window).width ()*.8;
+	
+	//Formulario Twitter
+	$("#form-twitter-legislador").colorbox({
+		iframe : true,
+		innerWidth : w,
+		innerHeight : h
+	});
+	//Formulario Sumate
+	$("#form-sumate").colorbox({
+		iframe : true,
+		innerWidth : w,
+		innerHeight : h
+	});
+});
