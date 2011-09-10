96f80531f12d2f22ae6f2cf6a695c0b43445bec8
etagwerker <ernesto@etagwerker.com>
2011-09-10 12:00:55 -0300

    Agregué datos de la base para la home.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,4 +2,11 @@ class StaticController < ApplicationController
   def quienes_somos
     
   end
+  
+  def home
+    @projects_count = Project.count
+    @diputados_count = Diputado.count
+    @senadores_count = Senator.count
+    @present_percentage = Legislador.total_present_percentage
+  end
 end
