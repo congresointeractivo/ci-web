c9671f1b824c225011949792c7b2b2edff9b3b3f
etagwerker <ernesto@etagwerker.com>
2011-10-10 15:14:28 -0300

    Saqué tests de métodos no soportados.

--- test/functional/political_parties_controller_test.rb
+++ test/functional/political_parties_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PoliticalPartiesControllerTest < ActionController::TestCase
   setup do
-    @political_party = political_parties(:one)
+    @political_party = PoliticalParty.first
   end
 
   test "should get index" do
@@ -11,39 +11,9 @@ class PoliticalPartiesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:political_parties)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create political_party" do
-    assert_difference('PoliticalParty.count') do
-      post :create, :political_party => @political_party.attributes
-    end
-
-    assert_redirected_to political_party_path(assigns(:political_party))
-  end
-
   test "should show political_party" do
     get :show, :id => @political_party.to_param
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, :id => @political_party.to_param
-    assert_response :success
-  end
-
-  test "should update political_party" do
-    put :update, :id => @political_party.to_param, :political_party => @political_party.attributes
-    assert_redirected_to political_party_path(assigns(:political_party))
-  end
-
-  test "should destroy political_party" do
-    assert_difference('PoliticalParty.count', -1) do
-      delete :destroy, :id => @political_party.to_param
-    end
-
-    assert_redirected_to political_parties_path
-  end
 end
--- test/functional/senators_controller_test.rb
+++ test/functional/senators_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SenatorsControllerTest < ActionController::TestCase
   setup do
-    @senator = senators(:one)
+    @senator = Senator.first
   end
 
   test "should get index" do
@@ -10,40 +10,9 @@ class SenatorsControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:senators)
   end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create senator" do
-    assert_difference('Senator.count') do
-      post :create, :senator => @senator.attributes
-    end
-
-    assert_redirected_to senator_path(assigns(:senator))
-  end
-
+  
   test "should show senator" do
     get :show, :id => @senator.to_param
     assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @senator.to_param
-    assert_response :success
-  end
-
-  test "should update senator" do
-    put :update, :id => @senator.to_param, :senator => @senator.attributes
-    assert_redirected_to senator_path(assigns(:senator))
-  end
-
-  test "should destroy senator" do
-    assert_difference('Senator.count', -1) do
-      delete :destroy, :id => @senator.to_param
-    end
-
-    assert_redirected_to senators_path
   end
 end
