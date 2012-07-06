6858e9dc3e064b57d36d758ef414a515d6409e6f
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2012-07-06 12:12:25 -0300

    se agrega la configuración del SMTP para producción

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,15 @@ CiWeb::Application.configure do
     :exception_recipients => %w{ci-errores@googlegroups.com}
 
   config.action_mailer.delivery_method = :smtp
-
+  
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => ENV['SMTP_USER_NAME'],
+    :password             => ENV['SMTP_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
 
